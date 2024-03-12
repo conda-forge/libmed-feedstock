438de782d4a7f9cafb4c7cfe0abc48a1c0b0a1d3
krande <kristoffer_andersen@outlook.com>
2024-03-12 15:44:23 +0100

    skip tests for now

--- recipe/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd tests
-
-# Compile the test program
-gcc -I$CONDA_PREFIX/include test_med_int_size.c -o test_med_int_size -lmed
-
-# Run the test and check output
-./test_med_int_size
